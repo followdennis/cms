c7d741e2ffa7b486e824ec2015b9b2c7214f8442
followdennis <963132814@qq.com>
2017-10-18 23:04:19 +0800

    laravel 5.5中使用vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,13 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
+ * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
 require('./bootstrap');
+
+window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -14,38 +16,11 @@ require('./bootstrap');
  */
 
 Vue.component('example', require('./components/Example.vue'));
-
-Vue.component(
-    'passport-clients',
-    require('./components/passport/Clients.vue')
-);
-
-Vue.component(
-    'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue')
-);
-
-Vue.component(
-    'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue')
-);
+import ElementUI from 'element-ui'    //引入element－ui
+import 'element-ui/lib/theme-default/index.css' //引入element－ui所需的css样式资源文件
 
 
-
+Vue.use(ElementUI)    //把引入的ElementUI装入我们的Vue
 const app = new Vue({
     el: '#app'
 });
-
-// window.Vue = require('vue');
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-//
-// Vue.component('example-component', require('./components/ExampleComponent.vue'));
-// // import Hello from './components/Hello.vue'; // 引入Hello 组件
-// // Vue.component('Hello',require('./components/Hello.vue'));
-// const app = new Vue({
-//     el: '#app'
-// });
