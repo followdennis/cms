fe458bdb51d12c091b81b4c3aceff44559d9f53c
followdennis <963132814@qq.com>
2017-10-23 20:07:17 +0800

    u

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -7,14 +7,14 @@
 
                     <div class="panel-body">
                        这是一个vue组件!
-<<<<<<< HEAD
+
                        你好，我是vdddad
                        <el-button>默认按钮</el-button>
                        <el-button type="primary">主要按钮</el-button>
                        <el-button type="text">按钮</el-button>
-=======
+
                         {{ message }}
->>>>>>> b15e8056307b6cccf0e4044794b4d667a054de03
+
                     </div>
                 </div>
             </div>
